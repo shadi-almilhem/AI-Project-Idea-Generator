0c025472b5499be4ea10f8f0ff561ffd02799bef
Shadi Al Milhem <almilhemshadi@gmail.com>
2024-09-27 13:51:01 +0400

    taost and textstyle edited

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AIProjectIdeaGenerator from "./AIProjectIdeaGenerator";
 
 export default function Home() {
   return (
-    <main className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <main className="grid items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <AIProjectIdeaGenerator />
     </main>
   );
